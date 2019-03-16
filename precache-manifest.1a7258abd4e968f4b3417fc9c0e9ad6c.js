f0bd37d0e9a5f2ee3599580d8f4562c477958ee8
Tomasz Pawęska <paweska.tomek@gmail.com>
2019-03-16 14:48:55 +0100

    my first aplication in

---
+++ precache-manifest.1a7258abd4e968f4b3417fc9c0e9ad6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b59806f59b778ce0b42",
+    "url": "/static/js/main.2b3e56c5.chunk.js"
+  },
+  {
+    "revision": "d604d6d590ceef29e39f",
+    "url": "/static/js/2.2c8f914b.chunk.js"
+  },
+  {
+    "revision": "3b59806f59b778ce0b42",
+    "url": "/static/css/main.3bb926b8.chunk.css"
+  },
+  {
+    "revision": "d604d6d590ceef29e39f",
+    "url": "/static/css/2.67a6e514.chunk.css"
+  },
+  {
+    "revision": "a830e598d1e8c2f759d8920e63e385db",
+    "url": "/index.html"
+  }
+];
